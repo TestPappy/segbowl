3a0aac75f791c19f8cadfe22839eb5825755b298
Patrick Prill <patrick@prill.net>
2023-04-09 09:00:07 +0200

    splitting unit test into individual tests

--- js/__tests__/persistence.test.js
+++ js/__tests__/persistence.test.js
@@ -2,7 +2,14 @@ import { loadDesign, saveDesign, checkStorage, clearDesign } from "../persistenc
 require('jest-localstorage-mock');
 
 describe('persistence can', () => {
-    it('save, load and clear a design', () => {
+    beforeEach(() => {
+        localStorage.clear();
+        localStorage.setItem.mockClear();
+        localStorage.getItem.mockClear();
+        localStorage.removeItem.mockClear();
+      });
+
+    it('save a design', () => {
         var bowlpropMock = {
             test: "123"
         }
@@ -10,13 +17,37 @@ describe('persistence can', () => {
         saveDesign(bowlpropMock);
         expect(Object.keys(localStorage.__STORE__).length).toBe(1);
         expect(localStorage.setItem).toHaveBeenLastCalledWith("bowlDesign", JSON.stringify(bowlpropMock));
-        // Checking...
+
+    });
+
+    it('can check for an existing design', () => {
+        var bowlpropMock = {
+            test: "123",
+            timestamp: "0815-4711"
+        };
+        localStorage.setItem("bowlDesign", JSON.stringify(bowlpropMock));
         var timestamp = checkStorage();
         expect(localStorage.getItem).toHaveBeenLastCalledWith("bowlDesign");
         expect(timestamp).toBe(bowlpropMock.timestamp);
-        // Loading...
+    });
+
+    it('load an existing design', () => {
+        var bowlpropMock = {
+            test: "123",
+            timestamp: "0815-4711"
+        };
+        localStorage.setItem("bowlDesign", JSON.stringify(bowlpropMock));
         var loadedBowlprop = loadDesign();
+        expect(localStorage.getItem).toHaveBeenLastCalledWith("bowlDesign");
         expect(loadedBowlprop.test).toBe("123");
+    });
+
+    it('remove an existing design', () => {
+        var bowlpropMock = {
+            test: "123",
+            timestamp: "0815-4711"
+        };
+        localStorage.setItem("bowlDesign", JSON.stringify(bowlpropMock));
         // Deleting...
         clearDesign();
         expect(Object.keys(localStorage.__STORE__).length).toBe(0);

06cc60f0b33dbe8b4202ebd81740be2485fd4d78
Patrick Prill <patrick@prill.net>
2023-04-13 12:24:24 +0200

    extract the remaining two bow calculator functions

---
+++ js/__tests__/ring_calculator.test.js
@@ -0,0 +1,58 @@
+import { Vector2 } from "three";
+import { dfltclrs, dfltlens } from "../common.mjs";
+import { calcRings, calcRingTrapz } from "../ring_calculator.mjs";
+
+var canvasinches = 8;
+var width = 500;
+var height = 500;
+var scale = width / canvasinches;
+var centerx = width / 2;
+var view2d = {
+    canvas: {
+        width: width,
+        height: width
+    },
+    centerx: centerx,
+    bottom: height - 0.5 * scale,
+    scale: scale
+};
+
+var bowlprop = {
+    cpoint: [
+        { x: centerx + 1.5 * scale, y: view2d.bottom },
+        { x: centerx + 2.0 * scale, y: view2d.bottom },
+        { x: centerx + 2.5 * scale, y: view2d.bottom - 2.5 * scale },
+        { x: centerx + 3.5 * scale, y: view2d.bottom - 3.0 * scale }, // This point is will also be start of next bezier curve
+    ],
+    rings: [{ height: .5, segs: 12, clrs: dfltclrs(), seglen: dfltlens(), xvals: [], theta: 0 },
+            { height: .5, segs: 14, clrs: dfltclrs(), seglen: dfltlens(), xvals: [], theta: 0 },
+            { height: .5, segs: 16, clrs: dfltclrs(), seglen: dfltlens(), xvals: [], theta: 0 },
+            { height: .5, segs: 18, clrs: dfltclrs(), seglen: dfltlens(), xvals: [], theta: 0 },
+    ],
+    curvesegs: 6,
+    thick: 0.25,
+    pad: 0.125,
+};
+
+describe('calcRings', () => {
+    it('calculates something with rings', () => {
+        calcRings(view2d, bowlprop);
+        expect(bowlprop.height).toBe(3.125);
+        expect(bowlprop.radius).toBe(3.5);
+        expect(bowlprop.usedrings).toBe(6);
+        expect(bowlprop.rings.length).toBe(6);
+        expect(Number.parseFloat(bowlprop.rings[1].xvals.max).toFixed(3)).toBe("2.257");
+        expect(Number.parseFloat(bowlprop.rings[1].xvals.min).toFixed(3)).toBe("1.780");
+        expect(Number.parseFloat(bowlprop.rings[3].xvals.max).toFixed(3)).toBe("2.607");
+        expect(Number.parseFloat(bowlprop.rings[3].xvals.min).toFixed(3)).toBe("2.079");
+        expect(Number.parseFloat(bowlprop.rings[5].xvals.max).toFixed(3)).toBe("3.625");
+        expect(Number.parseFloat(bowlprop.rings[5].xvals.min).toFixed(3)).toBe("2.833");
+    });
+});
+
+describe('calcRingTrapz', () => {
+    it('calculates this', () => {
+        calcRingTrapz(bowlprop, 1, true);
+        console.log(bowlprop);
+    });
+});
--- js/bowl.js
+++ js/bowl.js
@@ -7,6 +7,7 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { dfltclrs, dfltlens } from './common.mjs';
 import { screenToRealPoint, realToScreen, screenToReal, calcBezPath, splitRingY, offsetCurve } from './bowl_calculator.mjs';
+import { calcRings, calcRingTrapz } from './ring_calculator.mjs';
 import * as PERSISTENCE from './persistence.mjs';
 
 (() => {
@@ -75,7 +76,7 @@ import * as PERSISTENCE from './persistence.mjs';
         view2d.centerx = view2d.canvas.width / 2;
 
         bowlprop.cpoint = [
-            { x: view2d.centerx + 1.5 * view2d.scale, y: view2d.bottom },
+            { x: view2d.centerx + 1.0 * view2d.scale, y: view2d.bottom },
             { x: view2d.centerx + 2.0 * view2d.scale, y: view2d.bottom },
             { x: view2d.centerx + 2.0 * view2d.scale, y: view2d.bottom - 3.0 * view2d.scale },
             { x: view2d.centerx + 2.5 * view2d.scale, y: view2d.bottom - 3.5 * view2d.scale }, // This point is will also be start of next bezier curve
@@ -159,98 +160,6 @@ import * as PERSISTENCE from './persistence.mjs';
     }
 
     /*======================
-      Calculate bowl stuff
-    ======================*/
-    function calcRings() {
-        var ppp = calcBezPath(view2d, bowlprop);
-        var paths = offsetCurve(calcBezPath(view2d, bowlprop), bowlprop.thick / 2);
-
-        // Split path into components
-        var pathx1 = [], pathx2 = [], pathy1 = [], pathy2 = [];
-        for (var i = 0; i < paths.c1.length; i++) {
-            pathx1.push(paths.c1[i].x);
-            pathx2.push(paths.c2[i].x);
-            pathy1.push(paths.c1[i].y);
-            pathy2.push(paths.c2[i].y);
-        }
-
-        // Vertical profile
-        bowlprop.height = Math.max(Math.max.apply(null, pathy1), Math.max.apply(null, pathy2));
-        bowlprop.radius = Math.max(Math.max.apply(null, pathx1), Math.max.apply(null, pathx2));
-        var y = -bowlprop.thick / 2;
-        var i = 0;
-        while (y < bowlprop.height) {
-            var x = []; // x-values within this ring
-            var yidx = []; // INDEX of y-values in this ring
-            if (bowlprop.rings.length <= i) { // Need a new ring
-                bowlprop.rings.push({ height: .75, segs: 12, clrs: dfltclrs(), seglen: dfltlens(), xvals: [], theta: 0 });
-            }
-            for (var p = 0; p < pathx1.length; p++) {
-                if (pathy1[p] > y && pathy1[p] < y + bowlprop.rings[i].height) { x.push(pathx1[p]); yidx.push(p); }
-                if (pathy2[p] > y && pathy2[p] < y + bowlprop.rings[i].height) { x.push(pathx2[p]); yidx.push(p); }
-                if (p > 1 && pathy1[p - 1] < y && pathy1[p] > y + bowlprop.rings[i].height) {
-                    // ring is too thin, curve points jump over it.. interpolate.
-                    var m = (pathy1[p] - pathy1[p - 1]) / (pathx1[p] - pathx1[p - 1]);
-                    x.push((y - pathy1[p - 1]) / m + pathx1[p - 1]);
-                    yidx.push(p);
-                }
-                if (p > 1 && pathy2[p - 1] < y && pathy2[p] > y + bowlprop.rings[i].height) {
-                    var m = (pathy2[p] - pathy2[p - 1]) / (pathx2[p] - pathx2[p - 1]);
-                    x.push((y - pathy2[p - 1]) / m + pathx2[p - 1]);
-                    yidx.push(p);
-                }
-            }
-            bowlprop.rings[i].xvals = {
-                max: Math.max(0, Math.max.apply(null, x) + bowlprop.pad),
-                min: Math.max(0, Math.min.apply(null, x) - bowlprop.pad)
-            };
-            y += bowlprop.rings[i].height;
-            i += 1;
-        }
-        bowlprop.usedrings = i;
-    }
-
-    function calcRingTrapz(ringidx, rotate = true) {
-        if (ringidx == null) { ringidx = 0; }
-        var rotation = 0;
-        var trapzlist = [];
-        var thetas = [];
-        var maxtheta = [];
-        for (var segidx = 0; segidx < bowlprop.rings[ringidx].seglen.length; segidx++) {
-            maxtheta.push(Math.PI / bowlprop.rings[ringidx].segs * bowlprop.rings[ringidx].seglen[segidx]);
-        }
-        maxtheta = Math.max.apply(null, maxtheta);
-        var theta;
-        for (var segidx = 0; segidx < bowlprop.rings[ringidx].seglen.length; segidx++) {
-            thetas.push(rotation);
-            theta = Math.PI / bowlprop.rings[ringidx].segs * bowlprop.rings[ringidx].seglen[segidx];
-            var x2 = bowlprop.rings[ringidx].xvals.max * Math.cos(theta) / Math.cos(maxtheta); // cosines make different width segments meet at endpoints
-            var x1 = (bowlprop.rings[ringidx].xvals.min) * Math.cos(theta);
-            var y2 = x2 * Math.tan(theta);
-            var y1 = (bowlprop.rings[ringidx].xvals.min) * Math.sin(theta);
-            var trapz = [{ x: x1, y: y1 }, { x: x2, y: y2 }, { x: x2, y: -y2 }, { x: x1, y: -y1 }];
-            if (rotate) {
-                var rtrapz = [];
-                var toffset = bowlprop.rings[ringidx].theta; // + rotation;
-                for (var p = 0; p < trapz.length; p++) {
-                    // Complex-number magic to rotate segment around
-                    var zx = Math.cos(theta + rotation + toffset); // exp(j*theta*i) with j=complex
-                    var zy = Math.sin(theta + rotation + toffset);
-
-                    var rx = trapz[p].x * zx - trapz[p].y * zy; // Basically complex number multiplication
-                    var ry = trapz[p].y * zx + trapz[p].x * zy;
-                    rtrapz.push({ x: rx, y: ry });
-                }
-                trapz = rtrapz;
-            }
-            rotation += theta * 2;
-            trapzlist.push(trapz);
-        }
-        bowlprop.seltrapz = trapzlist;
-        bowlprop.selthetas = thetas;
-    }
-
-    /*======================
       Drawing functions
     ======================*/
     function clearCanvas(canvas, ctx) {
@@ -262,7 +171,7 @@ import * as PERSISTENCE from './persistence.mjs';
     }
 
     function drawSegProfile(ctx) {
-        calcRings();
+        calcRings(view2d, bowlprop);
         var y = -bowlprop.thick / 2;
         for (var i = 0; i < bowlprop.rings.length; i++) {
             ctx.beginPath();
@@ -336,7 +245,7 @@ import * as PERSISTENCE from './persistence.mjs';
     }
 
     function drawRing(ctx, selring) {
-        calcRingTrapz(selring, true);
+        calcRingTrapz(bowlprop, selring, true);
         for (var i = 0; i < bowlprop.rings[selring].segs; i++) {
             ctx.strokeStyle = "#000";
             ctx.lineWidth = 2;
@@ -425,7 +334,7 @@ import * as PERSISTENCE from './persistence.mjs';
     function build3D() {
         if (document.getElementById("canvas3d").style.display == 'none') { return; } // Don't calculate if not shown
         var curve = calcBezPath(view2d, bowlprop);
-        calcRings();
+        calcRings(view2d, bowlprop);
         var offcurve = offsetCurve(curve, bowlprop.thick / 2);
         for (var m = 0; m < view3d.mesh.length; m++) {
             view3d.mesh[m].geometry.dispose();
@@ -894,7 +803,7 @@ import * as PERSISTENCE from './persistence.mjs';
     function getReportSegsList(ring) {
         var donesegs = [];
         var seginfo = [];
-        calcRingTrapz(ring, false);
+        calcRingTrapz(bowlprop, ring, false);
         for (var seg = 0; seg < bowlprop.rings[ring].segs; seg++) {
             var idx = donesegs.indexOf(bowlprop.rings[ring].seglen[seg]);
             if (idx == -1) {
